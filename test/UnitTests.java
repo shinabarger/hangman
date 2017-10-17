1f7395ba9b4ab00fd62ad7e93dc1c10cf9e69ee1
shinabarger <davidshinabarger@gmail.com>
2017-10-16 20:29:39 -0400

    Initial commit

---
+++ test/UnitTests.java
@@ -0,0 +1,19 @@
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by davidshinabarger on 10/16/17.
+ */
+
+public class UnitTests {
+
+    @Test
+    public void firstTest() {
+        String expected = "Test";
+        String actual = "Test";
+        assertEquals(expected, actual);
+    }
+
+}
